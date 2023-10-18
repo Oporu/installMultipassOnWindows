c0cfae18da12bfce8e11e4d1b057ca2dfad4308b
Oporu <70465701+Oporu@users.noreply.github.com>
2023-10-18 23:24:53 +0800

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,7 @@
+curl -o pythonInstaller.exe "https://www.python.org/ftp/python/3.12.0/python-3.12.0.exe"
+curl -o virtualboxInstaller.exe "https://download.virtualbox.org/virtualbox/7.0.12/VirtualBox-7.0.12-159484-Win.exe"
+curl -L -o mutlipassInstaller.exe "https://multipass.run/download/windows"
+pythonInstaller
+python -m pip install --upgrade pywin32
+virtualboxInstaller
+multipassInstaller
