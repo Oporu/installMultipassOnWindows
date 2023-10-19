54537924f1ddffb11b961b213c8c03ca3f41f207
Oporu <70465701+Oporu@users.noreply.github.com>
2023-10-19 11:15:12 +0800

    still testing

---
+++ install.cmd
@@ -0,0 +1,17 @@
+# install choco
+powershell "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+
+# install C++ stuff & python
+choco install vcredist140 python3 -y
+# choco install vcredist-all python3 -y
+
+# pywin32 required for virtualbox
+python -m pip install --upgrade pywin32
+
+#
+choco install virtualbox -y
+choco install multipass -y --params="'/HyperVisor:VirtualBox'"
+
+# launches a new ubuntu server and get in shell of it
+multipass launch --name yee
+multipass shell yee
