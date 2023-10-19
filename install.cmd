b206e4557fb3a190bd79a6804cf8c1568a3b9cf4
Oporu <70465701+Oporu@users.noreply.github.com>
2023-10-19 00:00:29 -0500

    Update install.cmd
    refreshenv

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 # install choco
 powershell "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-
+refreshenv
 # install C++ stuff & python
 choco install vcredist140 python3 -y
 # choco install vcredist-all python3 -y
