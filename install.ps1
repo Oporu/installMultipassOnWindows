b227349c911c242efb8f61be5ab10093fa88df7b
Oporu <70465701+Oporu@users.noreply.github.com>
2023-10-19 10:45:22 +0800

    better comments

--- install.ps1
+++ install.ps1
@@ -1,9 +1,12 @@
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# install C++ stuff
-
+# install C++ stuff & python
 choco install vcredist140 python3 -y
 # choco install vcredist-all python3 -y
+
+# pywin32 required for virtualbox
 python3 -m pip install --upgrade pywin32
+
+#
 choco install virtualbox multipass -y
