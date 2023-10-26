3a1d512a4d69ae8705eab4064cba42fe4431bcb8
Oporu <70465701+Oporu@users.noreply.github.com>
2023-10-26 10:29:03 +0800

    Update installThenUpgradeV.ps1

--- installThenUpgradeV.ps1
+++ installThenUpgradeV.ps1
@@ -7,7 +7,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 #choco install Microsoft-Hyper-V --source windowsfeatures
 choco install multipass -y
 
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
 # launches a new ubuntu server -> apt upgrade -> getin shell of it
 multipass launch --name yee
 multipass exec yee sudo apt update
